2f8d29abc1f9a34b8b9da2ee0f118233292260bd
Haira Lei <darhaii13@gmail.com>
2022-07-02 13:26:38 +0800

    All features are working without storage

---
+++ src/components/FeedbackStats.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function FeedbackStats({ feedback }) {
+  let average =
+    feedback.reduce((acc, curr) => {
+      return acc + curr.rating;
+    }, 0) / feedback.length;
+
+  return (
+    <div className="feedback-stats">
+      <h4>{feedback.length} Reviews</h4>
+      <h4>
+        Average Rating:{' '}
+        {isNaN(average) ? 0 : average.toFixed(1).replace(/[.,]0$/, '')}
+      </h4>
+    </div>
+  );
+}
+
+FeedbackStats.propTypes = {
+  feedback: PropTypes.array.isRequired,
+};
+
+export default FeedbackStats;
