1c87372ed2ad1e0e1d22f3afbcf03ad23e9528d8
Haira Lei <darhaii13@gmail.com>
2022-07-02 22:19:02 +0800

    Add edit feature and add about page

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,8 +1,11 @@
 import FeedbackItem from './FeedbackItem';
-import PropTypes from 'prop-types';
 import { motion, AnimatePresence } from 'framer-motion';
+import { useContext } from 'react';
+import FeedbackContext from '../context/FeedbackContext';
 
-function FeedbackList({ feedback, handleDelete }) {
+function FeedbackList() {
+  const { feedback } = useContext(FeedbackContext);
+
   if (!feedback || feedback.length === 0) {
     return <p>No feedback yet</p>;
   }
@@ -12,12 +15,13 @@ function FeedbackList({ feedback, handleDelete }) {
       <AnimatePresence>
         {feedback.map(item => (
           <>
-            <motion.div key={item.id} initial={{opacity: 0}} animate={{opacity: 1}} exit={{opacity:0}}>
-              <FeedbackItem
-                key={item.id}
-                item={item}
-                handleDelete={handleDelete}
-              />
+            <motion.div
+              key={item.id}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+            >
+              <FeedbackItem key={item.id} item={item} />
             </motion.div>
           </>
         ))}
@@ -25,15 +29,5 @@ function FeedbackList({ feedback, handleDelete }) {
     </div>
   );
 }
-
-FeedbackList.propTypes = {
-  feedback: PropTypes.arrayOf(
-    PropTypes.shape({
-      // id: PropTypes.number.isRequired,
-      text: PropTypes.string.isRequired,
-      rating: PropTypes.number.isRequired,
-    })
-  ),
-};
 
 export default FeedbackList;
